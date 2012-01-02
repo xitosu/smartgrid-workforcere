56f6d0ed9c010ec67e7856f285d030051ab24c6e
Kim Holmes <zorianak@me.com>
2012-01-01 17:41:09 -0800

    Drop-down menu for CSUs added

---
+++ smartgrid/csus.php
@@ -0,0 +1,48 @@
+<?
+include_once "support_functions.php";
+
+//gets the header function (support_functions.php)
+getHeader();
+
+//navigation (support_functions.php)
+getNav();
+
+//sidebar (support_functions.php)
+getSide();
+
+//content goes here now; for now it is HTML but later we can format it into PHP if we want.
+ucContent();
+
+getFooter();
+
+function ucContent(){
+    $html = "";
+	$html .= "<div class=\"grid_13 homepage\">\n";
+	$html .= "<h1>University of California</h1>\n";
+	$html .= "<p></p>\n";
+	$html .= "	<form id=\"schoolForm\" class=\"grid_14\">\n";
+	$html .= "		<select id=\"schools\" onchange=\"addClasses2()\">\n";
+	$html .= "			<option name=\"select\" id=\"select\">Select a School</option>\n";
+	$html .= "			<option name=\"chanis\" id=\"chanis\">CSU Channel Islands</option>\n";
+	$html .= "			<option name=\"chico\" id=\"chico\">CSU Chico</option>\n";
+	$html .= "			<option name=\"domhill\" id=\"domhill\">CSU Dominguez Hills</option>\n";
+	$html .= "			<option name=\"ebay\" id=\"ebay\">CSU East Bay</option>\n";
+	$html .= "			<option name=\"fresno\" id=\"fresno\">CSU Fresno</option>\n";
+	$html .= "			<option name=\"fullerton\" id=\"fullerton\">CSU Fullerton</option>\n";
+	$html .= "			<option name=\"humboldt\" id=\"humboldt\">CSU Humboldt</option>\n";
+	$html .= "			<option name=\"lbeach\" id=\"lbeach\">CSU Long Beach</option>\n";
+	$html .= "			<option name=\"csula\" id=\"csula\">CSU Los Angeles</option>\n";
+	$html .= "		</select>\n";
+	$html .= "	</form>\n";
+		
+	$html .= "<div class=\"grid_13\" id=\"resultsdiv\">\n";
+	
+	$html .= "</div>\n";
+	
+	$html .= "</div>\n";
+	
+	$html .= "<!-- close the main content area -->\n";
+	
+	echo $html;
+}
+?>
