ba2273e6b86baeb9a7a6c9d9998f1b58e5070422
nikitha thouta <nikitha.thouta@gmail.com>
2012-02-28 14:50:03 -0800

    Put a test comment. i have added 3 drop down menus in this file..

--- smartgrid/highschoolgrads.php
+++ smartgrid/highschoolgrads.php
@@ -17,6 +17,7 @@ indexContent();
 getFooter();
 
 function indexContent() {
+//adding a comment
 	$html = "";
 
 	$html .="<div class=\"grid_13\">";
@@ -29,6 +30,61 @@ function indexContent() {
 	$html .="<p>One who practices engineering is called an engineer, and those licensed to do so may have more formal designations such as Professional Engineer, Chartered Engineer, Incorporated Engineer, Ingenieur or European Engineer. The broad discipline of engineering encompasses a range of more specialized sub disciplines, each with a more specific emphasis on certain fields of application and particular areas of technology.</p>\n\n";
 
 	$html .= "</div>\n";
+  
+        $html .="<div class=\"grid_14\">";
+
+        $html .="<p>What field are you interested in?</p>";
+
+        $html .="<select name="states">";
+
+        $html .="<option value="0">Choose a state</option>";
+
+        $html .="<option value="1">NORTH EAST</option>";
+
+        $html .="<option value="2">MID WEST</option>";
+
+        $html .="<option value="3">SOUTH</option>";
+
+        $html .="<option value="4">WEST</option>";
+
+        $html .="</select>";
+
+	$html .= "</div>\n";
+
+        $html .="<div class=\"grid_15\">";
+
+        $html .="<p>What field are you interested in?</p>";
+
+        $html .="<select name= "interested fields" class = "menu">";
+
+        $html .="<option value= "Select Field">Select Field</option>";
+
+        $html .="<option value= "Power Engineering">Power Engineering</option>";
+
+        $html .="<option value= "Electrical Engineering">Electrical Engineering</option> ";
+
+        $html .="</select>";
+
+	$html .= "</div>\n";
+
+        $html .="<div class=\"grid_16\">";
+
+        $html .="<p>What type of school are you interested in?</p>";
+
+        $html .="<select name= "school">";
+
+        $html .="<option value= "Select School">Select School</option>";
+
+        $html .="<option value= "University of California">University of California</option>";
+
+        $html .="<option value= "California State University">California State University</option>";
+
+        $html .="<option value= "California Community College">California Community College</option>";
+
+        $html .="</select>";
+
+	$html .= "</div>\n";
+
 	echo $html;
 }
 
