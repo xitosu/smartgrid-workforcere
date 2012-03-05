fffd7835681b6c2c0b209d0993ce567164cf2514
Kim Holmes <zorianak@me.com>
2012-03-04 21:41:53 -0800

    Took out extraneous divs from drop-down menus.

--- smartgrid/highschoolgrads.php
+++ smartgrid/highschoolgrads.php
@@ -24,14 +24,6 @@ function indexContent() {
 	
 	$html .="<br />";
 	$html .="<h1>Current High School Student</h1>";
-	
-	$html .="<p>Engineering is the discipline, art, skill and profession of acquiring and applying scientific, mathematical, economic, social, and practical knowledge, in order to design and build structures, machines, devices, systems, materials and processes.</p>\n";
-	
-	$html .="<p>One who practices engineering is called an engineer, and those licensed to do so may have more formal designations such as Professional Engineer, Chartered Engineer, Incorporated Engineer, Ingenieur or European Engineer. The broad discipline of engineering encompasses a range of more specialized sub disciplines, each with a more specific emphasis on certain fields of application and particular areas of technology.</p>\n\n";
-
-	$html .= "</div>\n";
-  
-        $html .="<div class=\"grid_14\">";
 
         $html .="<p>What field are you interested in?</p>";
 
@@ -49,9 +41,6 @@ function indexContent() {
 
         $html .="</select>";
 
-	$html .= "</div>\n";
-
-        $html .="<div class=\"grid_15\">";
 
         $html .="<p>What field are you interested in?</p>";
 
@@ -64,10 +53,6 @@ function indexContent() {
         $html .="<option value=\"Electrical Engineering\">Electrical Engineering</option> ";
 
         $html .="</select>";
-
-	$html .= "</div>\n";
-
-        $html .="<div class=\"grid_16\">";
 
         $html .="<p>What type of school are you interested in?</p>";
 
