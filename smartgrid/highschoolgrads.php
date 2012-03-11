2e119310902d0e4909caa5a33195e9b87ac78194
Kim Holmes <zorianak@me.com>
2012-03-11 00:31:04 -0800

    Fixing smartgit.  No big changes made, other than PHP-izing files.

--- smartgrid/highschoolgrads.php
+++ smartgrid/highschoolgrads.php
@@ -17,18 +17,55 @@ indexContent();
 getFooter();
 
 function indexContent() {
+//adding a comment
 	$html = "";
 
 	$html .="<div class=\"grid_13\">";
 	
 	$html .="<br />";
 	$html .="<h1>Current High School Student</h1>";
-	
-	$html .="<p>Engineering is the discipline, art, skill and profession of acquiring and applying scientific, mathematical, economic, social, and practical knowledge, in order to design and build structures, machines, devices, systems, materials and processes.</p>\n";
-	
-	$html .="<p>One who practices engineering is called an engineer, and those licensed to do so may have more formal designations such as Professional Engineer, Chartered Engineer, Incorporated Engineer, Ingenieur or European Engineer. The broad discipline of engineering encompasses a range of more specialized sub disciplines, each with a more specific emphasis on certain fields of application and particular areas of technology.</p>\n\n";
+
+        $html .="<p>What field are you interested in?</p>";
+
+</form name = "myform"> 
+
+        $html .="<select name=\"select1\">";
+
+        $html .="<option value=\"0\">Select Region</option>";
+
+        $html .="<option value=\"1\">NORTH EAST</option>";
+
+        $html .="<option value=\"2\">MID WEST</option>";
+
+        $html .="<option value=\"3\">SOUTH</option>";
+
+        $html .="<option value=\"4\">WEST</option>";
+
+        $html .="</select>";
+
+        $html .="<p>What type of school are you interested in?</p>";
+
+        $html .="<select name=\"select2\" size=\"1\" style=\"width: 200px\" > ";
+
+        $html .="<option value = \"0\"></option>;
+
+        $html .="</select>";
+
+
+        $html .="<p>What field are you interested in?</p>";
+
+        $html .="<select name=\"interested fields\" class =\"menu\">";
+
+        $html .="<option value=\"Select Field\">Select Field</option>";
+
+        $html .="<option value=\"Power Engineering\">Power Engineering</option>";
+
+        $html .="<option value=\"Electrical Engineering\">Electrical Engineering</option> ";
+
+        $html .="</select>";
 
 	$html .= "</div>\n";
+
 	echo $html;
 }
 
