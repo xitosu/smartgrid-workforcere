1e2d17b5045864460d3046efc0cf04d538c7e466
Carissa Wong <xcarissawongx@gmail.com>
2012-03-11 23:33:11 -0700

    Updated due to what might work with workshop

--- smartgrid/dbdata/mysqlmaker-courses.pl
+++ smartgrid/dbdata/mysqlmaker-courses.pl
@@ -1,4 +1,4 @@
-# mysqlMaker.pl, A program that takes CSV files and puts them into
+# mysqlMaker-courses.pl, A program that takes excel.CSV files and puts them into
 # a MySQL database
 
 open(INFILE, $ARGV[0]);
@@ -6,17 +6,23 @@ open(INFILE, $ARGV[0]);
 
 while (<INFILE>)
 {
-    chomp;
+    chomp @values; #yummy!
     @values = split('\t', $_); #splits by tabs
-    print "INSERT INTO course (course_id,dept,enrolled,class_name,instructor) ";
+    print "INSERT INTO course(course_number,school_number,course_title,course_description,course_active,course_region,course_type) ";
     print "values(";
-    print "$values[0],"; #gets the course ID
-    print "'$values[1]',"; #gets department
-    #$values[3] =~ s/"/'/g; #replaces "" with '' for professors
-    print "$values[3],"; #gets enrollment total
-    $values[2] =~ s/[a-zA-Z]//g;
-    print "$values[2],"; #gets course name/number
-    print "$values[4]"; #gets faculty name
+    print "$values[0]"; #gets Course Number
+	print "','";
+    print "$values[1]"; #gets School Number
+	print "','";
+    print "$values[2]"; #gets Course Title
+	print "','";
+    print "$values[3]"; #gets Course Description
+	print "','";
+    print "$values[4]"; #gets Course Active
+	print "','";
+    print "$values[5]"; #Course Region
+	print "','";
+    print "$values[6]"; #Course Type
     print"); \n";
 }
 
