6eee2a4e25193bc5e30e668578f4012e6b64fd81
Kim Holmes <zorianak@me.com>
2012-03-22 01:29:46 -0700

    Fixed some PHP syntax errors with strings.  Fixed the drop-downs; someone had added menus that didn't need to be there.  Added a temporarily disabled "submit" button.  Formatted form.

--- smartgrid/highschoolgrads.php
+++ smartgrid/highschoolgrads.php
@@ -20,7 +20,7 @@ function indexContent() {
 //adding a comment
 	$html = "";
 
-	$html = "<style type="text/css">";
+	$html = "<style type=\"text/css\">";
 
 	$html = ".hiddenMenu {display: none;}";
 
@@ -33,107 +33,56 @@ function indexContent() {
 	$html .="<br />";
 	$html .="<h1>Current High School Student</h1>";
 
-<form action=" " method="post" enctype="multipart/form-data">
-
-        $html .="<label for=\"select region\">Select Region </label>";
-
-        $html .="<select name=\"category\" id=\"region\" style=\"width: 200px\" onchange=\"showDiv(this.value);\">";
-
-        $html .="<option value=\" \">Select Region</option>";
-
-        $html .="<option value=\"1\">NORTH EAST</option>";
-
-        $html .="<option value=\"2\">MID WEST</option>";
-
-        $html .="<option value=\"3\">SOUTH</option>";
-
-        $html .="<option value=\"4\">WEST</option>";
-
+	//form for determing what track to send them on.
+	$html .="<form action=\"highschoolgrads.php\" method=\"post\" enctype=\"multipart/form-data\">";
+     
+	 //drop down menu for region
+	 $html .="<div class=\"form-input\">";
+	 $html .="<div class=\"float-left grid_6\">";
+	 $html .="<label id=\"select region\">Please select a region </label>";
+	 $html .= "</div>\n";
+	 $html .="<div class=\"float-left grid_6\">";
+        $html .="<select id=\"category\" id=\"region\" style=\"width: 200px\" onchange=\"showDiv(this.value);\">";
+        $html .="<option value=\"select_region\">Select Region</option>";
+        $html .="<option value=\"Northern\">Northern</option>";
+        $html .="<option value=\"Central\">Central</option>";
+        $html .="<option value=\"Southern\">Southern</option>";
         $html .="</select>";
+		$html .= "</div><div style=\"clear:both;\"></div></div>\n";
 
-        $html .="<p id=\"1\" class=\"hiddenMenu\">";
-
-        $html .="<label for=\"select_school\">Select NORTH EAST region schools </label>
-
-        $html .="<select name=\"subjectCategory\" style=\"width: 200px\" id=\"select school\">";
-
-        $html .="<option value=\" \">Select School</option>";
-
-        $html .="<option value=\"UC\">University of California</option>";
-
-        $html .="<option value=\"CSU\">California State University</option>";
-
-        $html .="<option value=\"CCC\">California Community College</option>";
-
-        $html .="</select>";
-
-        $html .="</p>";
-
-        $html .="<p id=\"2\" class=\"hiddenMenu\">";
-
-        $html .="<label for=\"select_school\">Select MID WEST region schools </label>
-
-        $html .="<select name=\"subjectCategory\" style=\"width: 200px\" id=\"select school\">";
-
-        $html .="<option value=\" \">Select School</option>";
-
-        $html .="<option value=\"UC\">University of California</option>";
-
-        $html .="<option value=\"CSU\">California State University</option>";
-
-        $html .="<option value=\"CCC\">California Community College</option>";
-
-        $html .="</select>";
-
-        $html .="</p>";
-
-        $html .="<p id=\"3\" class=\"hiddenMenu\">";
-
-        $html .="<label for=\"select_school\">Select SOUTH region schools </label>
-
-        $html .="<select name=\"subjectCategory\" style=\"width: 200px\" id=\"select school\">";
-
-        $html .="<option value=\" \">Select School</option>";
-
-        $html .="<option value=\"UC\">University of California</option>";
-
-        $html .="<option value=\"CSU\">California State University</option>";
-
-        $html .="<option value=\"CCC\">California Community College</option>";
-
-        $html .="</select>";
-
-        $html .="</p>";
-
-        $html .="<p id=\"1\" class=\"hiddenMenu\">";
-
-        $html .="<label for=\"select_school\">Select WEST region schools </label>
-
-        $html .="<select name=\"subjectCategory\" style=\"width: 200px\" id=\"select school\">";
-
-        $html .="<option value=\" \">Select School</option>";
-
-        $html .="<option value=\"UC\">University of California</option>";
-
-        $html .="<option value=\"CSU\">California State University</option>";
-
-        $html .="<option value=\"CCC\">California Community College</option>";
-
-        $html .="</select>";
-
-        $html .="</p>";
-
-        $html .="<p>What field are you interested in?</p>";
-
-        $html .="<select name=\"interested fields\" class =\"menu\">";
-
+		//drop down for field of interest
+		$html .="<div class=\"form-input\">";
+		$html .="<div class=\"float-left grid_6\">";
+        $html .="<label>Please select a field of interest:</label>";
+		$html .= "</div>\n";
+		$html .="<div class=\"float-left grid_6\">";
+        $html .="<select id=\"interested fields\" class =\"menu\">";
         $html .="<option value=\"Select Field\">Select Field</option>";
-
-        $html .="<option value=\"Power Engineering\">Power Engineering</option>";
-
-        $html .="<option value=\"Electrical Engineering\">Electrical Engineering</option> ";
-
+        $html .="<option value=\"mechanical_engineering\">Mechanical Engineering</option>";
+        $html .="<option value=\"electrical_engineering\">Electrical Engineering</option>";
         $html .="</select>";
+		$html .= "</div><div style=\"clear:both;\"></div></div>\n";
+		
+		//program type
+		$html .="<div class=\"form-input\">";
+		$html .="<div class=\"float-left grid_6\">";
+		$html .="<label>Please select the type of program you are interested in:</label>";
+		$html .= "</div>\n";
+		$html .="<div class=\"float-left grid_6\">";
+        $html .="<select id=\"interested fields\" class =\"menu\">";
+        $html .="<option value=\"select_program\">Select Program</option>";
+        $html .="<option value=\"cert\">Certification Program</option>";
+        $html .="<option value=\"twoyear\">Two Year Program</option>";
+		$html .="<option value=\"fouryear\">Four Year University</option>";
+        $html .="</select>";
+		$html .= "</div><div style=\"clear:both;\"></div></div>\n";
+		
+		//select button to submit this information
+		$html .="<div class=\"submit_button\">";
+		$html .="<input type=\"button\" disabled=\"true\" id=\"track_submit\" value=\"Complete form to submit!\" />";
+		$html .="</div>";
+		
+	$html .="</form>";
 
 	$html .= "</div>\n";
 
