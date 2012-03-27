1c0e36821a40b70de58304484f82dbffe1747492
Carissa Wong <xcarissawongx@gmail.com>
2012-03-27 10:39:33 -0700

    Courses.txt was not changed from before. mysqlmaker-courses.pl was changed to commit what was previously changed OC.txt results of combining Courses.txt and mysqlmaker-courses.pl

--- smartgrid/dbdata/mysqlmaker-courses.pl
+++ smartgrid/dbdata/mysqlmaker-courses.pl
@@ -7,26 +7,14 @@ while (<INFILE>)
 {
     chomp @values; #yummy!
     @values = split('\t', $_); #splits by tabs
-<<<<<<< HEAD
-    print "INSERT INTO course(course_number,school_number,course_title,course_description,course_active,course_type) ";
-    print "values(";
-    print "'$values[0]',"; #gets Course Number
-    print "'$values[1]',"; #gets School Number
-    print "$values[2],"; #gets Course Title
-    print "$values[3],"; #gets Course Description
-    print "'$values[4]',"; #gets Course Active
-    print "$values[5]"; #Course Type
-    print"); \n";
-=======
     print "INSERT INTO smartgrid.courses(school_number,course_title,course_description,course_active,course_type) ";
     print "values(";
     print "'$values[0]',"; #get course number
     print "'$values[1]',"; #gets School Number
     print "\'$values[2]\',"; #gets Course Title
     print "\'$values[3]\',"; #gets Course Description
-    print "'$values[4]',"; #gets Course Active
+    print "'$values[4]'"; #gets Course Active
     print ");\n";
->>>>>>> origin/master
 }
 
 close (INFILE);
